a1c980bf1b5e53c19e916200d4a98866ccb28e4f
minkhantkoo <minkhant5771@gmail.com>
2023-07-20 11:31:58 +0630

    different theme settings and chart added

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -16,6 +16,12 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (context, index) => Dismissible(
         onDismissed: (direction) => remove(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.5),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
         key: ValueKey(expenses[index]),
         child: ExpenseItem(expenses[index]),
       ),
